7dc6c42081d05171d11a1d6abc93609811c05055
Roksana Zalesskaia <roksanazalesskaia@gmail.com>
2025-09-03 18:31:38 +0300

    basics_exceptions_lesson4 done

---
+++ src/main/java/school/sorokin/javacore/exceptions/lesson4/RoomNotAvailableException.java
@@ -0,0 +1,7 @@
+package school.sorokin.javacore.exceptions.lesson4;
+
+public class RoomNotAvailableException extends Exception {
+    public RoomNotAvailableException(String message) {
+        super(message);
+    }
+}
